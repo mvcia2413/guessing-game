a2c3933b7bbf28a2c7ba8699c0892f24f51cc9c4
Julianne <juliaguas17@gmail.com>
2022-12-22 21:35:28 -0600

    in-class work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
 // start here
+
+//putting div tag into a variable
+var text = document.getElementsByClassName(".word-blanks")
+console.log(text)
+
+//start game by clicking button
+var startButton = document.querySelector("#start-button")
+startButton.addEventListener("click", function() {
+    startGame()
+    }
+)
+
+function StartGame() {
+    document.addEventListener("keydown", function(event) {
+    var key = event.key;
+    if (key=="A" || key == "P" || key == "I") {
+
+    }
+    }
+    )
+}
